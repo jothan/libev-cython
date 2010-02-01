4f050b0ae58cb8c0b146cd3d6e2e672f7a4ec66c
Jonathan Bastien-Filiatrault <joe@x2a.org>
2010-01-31 21:50:54 -0500

    Add license headers.

--- libev/global.pyx
+++ libev/global.pyx
@@ -1,3 +1,19 @@
+# libev-cython: libev wrapper for Python
+# Copyright (C) 2010  Jonathan Bastien-Filiatrault
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 cimport libev.capi as capi
 cimport python_exc
 import math
